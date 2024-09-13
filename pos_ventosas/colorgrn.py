a5fdfa8d9790b5de98d3b5e3340ac6caa03d2d19
fm-puchades <fmartinez@tcicutting.com>
2024-09-13 18:23:45 +0200

    VENTOSAS v.3

---
+++ pos_ventosas/colorgrn.py
@@ -0,0 +1,65 @@
+#
+
+# https://gradients.app/es/newcolorpalette/f18ed9-ff939e-ffb270-d7d66e-8ef1a6/
+
+import random
+
+def hsl_to_rgb(h, s, l):
+    c = (1 - abs(2 * l - 1)) * s
+    x = c * (1 - abs((h / 60) % 2 - 1))
+    m = l - c / 2
+    r, g, b = 0, 0, 0
+
+    if 0 <= h < 60:
+        r, g, b = c, x, 0
+    elif 60 <= h < 120:
+        r, g, b = x, c, 0
+    elif 120 <= h < 180:
+        r, g, b = 0, c, x
+    elif 180 <= h < 240:
+        r, g, b = 0, x, c
+    elif 240 <= h < 300:
+        r, g, b = x, 0, c
+    elif 300 <= h < 360:
+        r, g, b = c, 0, x
+
+    r = (r + m) * 255
+    g = (g + m) * 255
+    b = (b + m) * 255
+
+    return int(r), int(g), int(b)
+
+def rgb_to_hex(r, g, b):
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+def generar_paleta_proxima_hex(tono_inicial, num_colores, proximidad=30, aleatorio=False):
+    """
+    Genera una paleta de colores basados en un tono inicial en la rueda de color HSL.
+
+    :param tono_inicial: El primer color en grados (Hue), entre 0 y 360.
+    :param num_colores: Número de colores a generar.
+    :param proximidad: Separación entre colores en grados (entre 0 y 360).
+    :param aleatorio: Si True, los colores se organizarán de forma aleatoria.
+    """
+    colores_hex = []
+    for i in range(num_colores):
+        hue = (tono_inicial + i * proximidad) % 360  # Colores próximos
+        r, g, b = hsl_to_rgb(hue, 1, 0.5)  # Saturación 1 y Luminosidad 0.5
+        color_hex = rgb_to_hex(r, g, b)
+        colores_hex.append(color_hex)
+
+    # Si se elige aleatorio, reorganizar la lista
+    if aleatorio:
+        random.shuffle(colores_hex)
+
+    return colores_hex
+
+# Ejemplo de uso:
+tono_inicial = 159  # Color inicial en HSL (verde)
+num_colores = 50     # Número de colores a generar
+proximidad = 3     # Separación entre colores en grados
+aleatorio = True    # Organizar la lista de colores de forma aleatoria
+
+paleta = generar_paleta_proxima_hex(tono_inicial, num_colores, proximidad, aleatorio)
+
+print(paleta)
---
+++ pos_ventosas/tets copy 2.py
@@ -0,0 +1,102 @@
+import json
+import cv2
+import numpy as np
+from os import sep
+
+formato = (800, 600)
+
+my_list = []
+_puntos = []
+_ventosas = []
+_circulos = []
+
+""" EJEMPLO
+ventosas = [
+      {
+    "diameter": 37,
+    "position": [
+      0,
+      0
+    ],
+    "force": 16.309887869520896,
+    "type": 1
+  },
+]
+"""
+
+"""
+contorno = {
+    "contour": [(-13, 97), (13, 97), (160, 21), (160, -21), (13, -97), (-13, -97), (-160, -21), (-160, 21)]
+}
+"""
+
+def Lienzo(x_px=800, y_px=600, grey_n=255):
+    """Creación de una imagen con una matríz
+       de x filas por y columnas y 3 canales (b,g,r).
+    Esta matriz se multiplica por 255 para hacerla blanca."""
+    if x_px == 0: x_px = 800
+    if y_px == 0: y_px = 600
+    imagen = grey_n*np.ones((y_px, x_px,3),dtype=np.uint8)
+    return imagen
+
+with open(f"HERRAMIENTA01{sep}tool01_entrada_ventosas.json") as v:
+    herramienta_ = json.load(v)
+
+
+ventosas = herramienta_["ventosas"]
+contorno = herramienta_["contorno"]
+
+print(">>>>>>>>>>>> contorno =:", type(contorno), contorno)
+
+ventosas_dict = dict(ventosas[0])
+contorno_dict = dict(contorno)
+
+for i in ventosas:        
+    x = float(i["position"][0]+formato[0]/2)
+    y = float(i["position"][1]+formato[1]/2)
+    x_ = i["position"][0]
+    y_ = i["position"][1]
+    position = {"position": [x_, y_]}
+    diameter = {"diameter": [i["diameter"]]}
+    type_ = {"type": [i["type"]]}
+    _ventosas.append([position, diameter, type_])
+    _circulos.append(((x, y), float(i["diameter"])))
+
+#####################################################################
+print("#"*50)
+print("contorno =",type(contorno), contorno)
+print("_ventosas =",type(_ventosas))
+for i in _ventosas:
+    print(i)
+
+#####################################################################
+
+my_list.append(_ventosas)
+my_list.append(contorno)
+
+with open(f"HERRAMIENTA01{sep}tool01_salida_contornos.json", "w") as f:
+
+    json.dump(my_list, f, indent=4)
+    f.close()
+
+#####################################################################
+imagen = Lienzo(x_px=formato[0], y_px=formato[1])
+
+for p in contorno:
+    x = p[0]+formato[0]/2
+    y = p[1]+formato[1]/2
+    p_c = (int(x), int(y))
+    _puntos.append(p_c)
+
+for c in _circulos:
+    print(">>>>>>>>>>>>>>> circulo=", c[0], c[1])
+    cv2.circle(imagen, (int(c[0][0]),int(c[0][1])), int(c[1]/2), (0,0,0), 2)
+
+puntos_array = np.array(_puntos)
+
+
+#cv2.drawContours(imagen, [contorno["contour"]], -1, (0, 0, 0), 2)
+cv2.drawContours(imagen, [puntos_array], -1, (0, 0, 0), 2)
+cv2.imshow("HERRAMIENTA", imagen)
+cv2.waitKey(5000)
+cv2.imwrite(f"HERRAMIENTA01{sep}tool01.png", imagen)
