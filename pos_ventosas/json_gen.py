eafb032745ec025ef211b421d451fb8ffec4d0c0
fm-puchades <fmartinez@tcicutting.com>
2024-09-26 18:23:38 +0200

    Ventosas v1.4

--- pos_ventosas/json_gen.py
+++ pos_ventosas/json_gen.py
@@ -21,6 +21,17 @@ def Lienzo(x_px=800, y_px=600, grey_n=255):
     imagen = grey_n*np.ones((y_px, x_px,3),dtype=np.uint8)
     return imagen
 
+def colorize(_type:int=0)->tuple:
+    if _type == 1:
+        color = (100, 200, 100)
+    elif _type == 2:
+        color = (255, 100, 100)
+    elif _type > 2:
+        color = (0, 0, 255)
+    else:
+        color = (55, 55, 55)
+    return color
+
 with open(f"HERRAMIENTA01{sep}tool01_entrada_ventosas.json") as v:
     herramienta_ = json.load(v)
 
@@ -28,10 +39,14 @@ with open(f"HERRAMIENTA01{sep}tool01_entrada_ventosas.json") as v:
 ventosas = herramienta_["ventosas"]
 contorno = herramienta_["contorno"]
 
-print(">>>>>>>>>>>> contorno =:", type(contorno), contorno)
+ventosas_dict = dict()
+ventosas_dict["tool"] = ventosas
 
-ventosas_dict = dict(ventosas[0])
-contorno_dict = dict(contorno)
+contorno_dict = dict()
+contorno_dict["geometry"] = contorno
+
+#print(">>>>>>>>>>>> TOOL =:", type(ventosas_dict), ventosas_dict)
+#print(">>>>>>>>>>>> GEOMETRY =:", type(contorno_dict), contorno_dict)
 
 for i in ventosas:        
     x = float(i["position"][0]+formato[0]/2)
@@ -42,7 +57,7 @@ for i in ventosas:
     diameter = {"diameter": [i["diameter"]]}
     type_ = {"type": [i["type"]]}
     _ventosas.append([position, diameter, type_])
-    _circulos.append(((x, y), float(i["diameter"])))
+    _circulos.append(((x, y), float(i["diameter"]), int(i["type"])))
 
 #####################################################################
 print("#"*50)
@@ -53,15 +68,18 @@ for i in _ventosas:
 
 #####################################################################
 
-my_list.append(_ventosas)
-my_list.append(contorno)
+#my_list.append(_ventosas)          # sin etiquetas
+#my_list.append(contorno)           # sin etiquetas
+my_list.append(ventosas_dict)       # con etiquetas
+my_list.append(contorno_dict)       # con etiquetas
 
 with open(f"HERRAMIENTA01{sep}{filename}.json", "w") as f:
-
     json.dump(my_list, f, indent=4)
     f.close()
 
 #####################################################################
+print("\n>> Dibujando...")
+
 imagen = Lienzo(x_px=formato[0], y_px=formato[1])
 
 for p in contorno:
@@ -70,8 +88,11 @@ for p in contorno:
     p_c = (int(x), int(y))
     _puntos.append(p_c)
 
+
 for c in _circulos:
-    print(">>>>>>>>>>>>>>> circulo=", c[0], c[1])
+    #print(">>>>>>>>>>>>>>> circulo=", c[0], c[1])
+    #print(">>>>>>>>>>>>>>> Type=", c[2])
+    cv2.circle(imagen, (int(c[0][0]),int(c[0][1])), int(c[1]/2), colorize(c[2]), -1)
     cv2.circle(imagen, (int(c[0][0]),int(c[0][1])), int(c[1]/2), (0,0,0), 2)
 
 puntos_array = np.array(_puntos)
