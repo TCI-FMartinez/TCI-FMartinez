edd1271b920f78080c8792600c7131656e198c62
fm-puchades <catarsis.75@gmail.com>
2024-11-25 19:11:11 +0100

    LPP parser v.4

---
+++ parser_lpp/modules/discretiza_arco copy.py
@@ -0,0 +1,92 @@
+
+###### EJEMPLO DE DISCRETIZACION DE ARCO ######
+
+import numpy as np
+import cv2
+
+
+codigo_g = """
+G65 P9102 A101 B01  ;
+G1X19.59Y191.54  ;
+G3X21.34Y193.29I0J1.75  ;
+G1X21.34Y373.15  ;
+G2X28.59Y380.4I7.25J0  ;
+G1X95.59Y380.4  ;
+G2X102.84Y373.15I0J-7.25  ;
+G1X102.84Y359.15  ;
+G2X99.59Y355.9I-3.25J0  ;
+G1X68.59Y355.9  ;
+G3X65.84Y353.15I0J-2.75  ;
+"""
+
+
+def DiscretizaArco (X_start, Y_start, X_end, Y_end, I, J, N = 10, horario = True) -> list:
+
+    # Centro absoluto
+    C_x = X_start + I
+    C_y = Y_start + J
+
+    # Radio
+    R = np.sqrt(I**2 + J**2)
+
+    # Ángulos inicial y final
+    theta_start = np.arctan2(Y_start - C_y, X_start - C_x)
+    theta_end = np.arctan2(Y_end - C_y, X_end - C_x)
+
+
+    ##vvvvvvvvvvvv AQUI HAY QUE DISTINGUIR SI ES INTERIOR O EXTERIOR PARA GIRAR A DERECHAS O IZQ  vvvvvvvvv
+    # Asegurarse de que theta_end es mayor que theta_start para un arco antihorario (G3)
+    if theta_end < theta_start and not horario:
+        theta_end += 2 * np.pi
+        delta_theta = (theta_start - theta_end) / N
+        # Si es horario, asegurarse de que theta_end es menor que theta_start
+    else:
+        #theta_start < theta_end:         ## and not is_G3
+        theta_start += 2 * np.pi
+        delta_theta = (theta_end - theta_start) / N
+
+
+    # Cálculo de puntos discretos
+    points = []
+    for k in range(N + 1):
+        theta = theta_start + k * delta_theta
+        X_k = C_x + R * np.cos(theta)
+        Y_k = C_y + R * np.sin(theta)
+        points.append((X_k, Y_k))
+
+    return points  # Lista de puntos discretos
+
+
+######################################################################
+
+
+if __name__ == "__main__":
+
+    codigo_g = """
+    G1X10Y4.75  ;
+    G2X4.75Y10I0J5.25  ;
+    G1X4.75Y50  ;
+    """
+    escala = 1
+    segmentos = 10
+    x_ini, y_ini, x_fin, y_fin, I1, J1 = 10, 4.75, 4.75, 10, 0, 5.25
+    
+    
+    segmentos_G3 = DiscretizaArco(x_ini, y_ini, x_fin, y_fin, I1, J1, segmentos, False)
+    #segmentos_G2 = DiscretizaArco(21.34, 373.15, 28.59, 380.4, 7.25, 0, segmentos, False)
+    
+    
+    # Crear una nueva imagen en blanco con un tamaño adecuado
+    image = np.ones((800, 800, 3), dtype=np.uint8) * 255  # Agregar margen
+    image = cv2.circle(image, (int(x_ini*escala),int(y_ini+J1*escala)), 2, (0,250,0), 2)
+    image = cv2.line(image,  (5, 5), (10, 5), (0,0,250), 1)
+    #image = cv2.circle(image, (int((x_ini + I1)*escala),int((y_ini+J1)*escala)), int(J1*escala), (0,0,0), 2) 
+    x1, y1 = x_ini, x_fin
+    color = (0,0,0)
+    for segment in segmentos_G3:
+        color = (color[0], color[1]+int(200/segmentos), color[2])
+        image = cv2.line(image, (int(x1*escala),int(y1*escala)), (int(segment[0]*escala), int(segment[1]*escala)), (0,0,250), 1)
+        x1, y1 = int(segment[0]*escala), int(segment[1]*escala)
+    image = cv2.line(image,  (5, 10), (5, 50), (0,0,250), 1)            
+    cv2.imshow("IMAGEN RESULTADO", image)
+    cv2.waitKey(0)
--- parser_lpp/modules/discretiza_arco.py
+++ parser_lpp/modules/discretiza_arco.py
@@ -2,102 +2,96 @@
 ###### EJEMPLO DE DISCRETIZACION DE ARCO ######
 
 import numpy as np
-import cv2
 
-
-codigo_g = """
-G65 P9102 A101 B01  ;
-G1X19.59Y191.54  ;
-G3X21.34Y193.29I0J1.75  ;
-G1X21.34Y373.15  ;
-G2X28.59Y380.4I7.25J0  ;
-G1X95.59Y380.4  ;
-G2X102.84Y373.15I0J-7.25  ;
-G1X102.84Y359.15  ;
-G2X99.59Y355.9I-3.25J0  ;
-G1X68.59Y355.9  ;
-G3X65.84Y353.15I0J-2.75  ;
-"""
-
-
-def DiscretizaArco (X_start, Y_start, X_end, Y_end, I, J, N = 10, horario = True) -> list:
-
-    # Centro absoluto
-    C_x = X_start + I
-    C_y = Y_start + J
-
-    # Radio
-    R = np.sqrt(I**2 + J**2)
-
-    # Ángulos inicial y final
-    theta_start = np.arctan2(Y_start - C_y, X_start - C_x)
-    theta_end = np.arctan2(Y_end - C_y, X_end - C_x)
-
-
-    ##vvvvvvvvvvvv AQUI HAY QUE DISTINGUIR SI ES INTERIOR O EXTERIOR PARA GIRAR A DERECHAS O IZQ  vvvvvvvvv
-    # Asegurarse de que theta_end es mayor que theta_start para un arco antihorario (G3)
-    if theta_end < theta_start and not horario:
-        theta_end += 2 * np.pi
-        # Si es horario, asegurarse de que theta_end es menor que theta_start
-    else:
-        #theta_start < theta_end:         ## and not is_G3
-        theta_start += 2 * np.pi
-
-    # Discretización
-    #N = 10  # Número de segmentos
+def DiscretizaArco(x1, y1, x2, y2, i, j, N=10, G2 = True, horario=True):
+    """
+    Discretiza un arco en N puntos intermedios.
     
-    delta_theta = (theta_end - theta_start) / N
+    :param x1, y1: Coordenadas del punto inicial del arco.
+    :param x2, y2: Coordenadas del punto final del arco.
+    :param i, j: Coordenadas relativas del centro del arco.
+    :param N: Número de segmentos a discretizar.
+    :param horario: Sentido del arco (True: horario, False: antihorario).
+    :return: Lista de puntos [(x, y), ...] que discretizan el arco.
+    """
+    # Calcular el centro absoluto del arco
+    cx = x1 + i
+    cy = y1 + j
 
-    # Cálculo de puntos discretos
-    points = []
+    # Calcular el radio del arco
+    r = np.sqrt(i**2 + j**2)
+
+    # Garantizamos que el numero mínimo de segmentos sea 3.
+    if N < 3: N = 3
+
+    if r > 5: N * 2    # Aumentamos en numero de discretización si se supera cierto radio
+
+    # Calcular los ángulos inicial y final
+    theta1 = np.arctan2(y1 - cy, x1 - cx)
+    theta2 = np.arctan2(y2 - cy, x2 - cx)
+
+    # Ajustar los ángulos para el sentido correcto
+    if G2:
+        if horario:
+            if theta2 > theta1: theta1 += 2 * np.pi  # Asegurar recorrido horario
+        else:
+            if theta2 < theta1:theta2 += 2 * np.pi  # Asegurar recorrido antihorario
+    elif not G2:
+        if horario:
+            if theta2 < theta1:
+                theta2 += 2 * np.pi  # Asegurar recorrido antihorario
+        else:
+            if theta2 > theta1: theta1 += 2 * np.pi  # Asegurar recorrido horario
+
+
+    # Generar los valores de theta discretizados
+    theta_values = []
+    step = (theta2 - theta1) / N
     for k in range(N + 1):
-        theta = theta_start + k * delta_theta
-        X_k = C_x + R * np.cos(theta)
-        Y_k = C_y + R * np.sin(theta)
-        points.append((X_k, Y_k))
+        theta_values.append(theta1 + k * step)
 
-    return points  # Lista de puntos discretos
+    # Generar los puntos discretizados
+    points = []
+    for theta in theta_values:
+        x = cx + r * np.cos(theta)
+        y = cy + r * np.sin(theta)
+        points.append((x, y))
+
+    return points
 
 
 ######################################################################
 
 
 if __name__ == "__main__":
+    import cv2
 
     codigo_g = """
-    G65 P9102 A101 B01  ;
-    G1X19.59Y191.54  ;
-    G3X21.34Y193.29I0J1.75  ;
-    G1X21.34Y373.15  ;
-    G2X28.59Y380.4I7.25J0  ;
-    G1X95.59Y380.4  ;
-    G2X102.84Y373.15I0J-7.25  ;
-    G1X102.84Y359.15  ;
-    G2X99.59Y355.9I-3.25J0  ;
-    G1X68.59Y355.9  ;
-    G3X65.84Y353.15I0J-2.75  ;
+    G1X10Y4.75  ;
+    G2X4.75Y10I0J5.25  ;
+    G1X4.75Y50  ;
     """
     escala = 1
-    segmentos = 5
-    x_ini, y_ini, x_fin, y_fin, I1, J1 = 50.5, 200.5, 70.5, 200.5, 0, 20
+    segmentos = 15
+    x_ini, y_ini, x_fin, y_fin, I1, J1 = 50, 50, 51, 51, 0, 50
     
-    segmentos_G3 = DiscretizaArco(x_ini, y_ini, x_fin, y_fin, I1, J1, segmentos)
+    
+    segmentos_G3 = DiscretizaArco(x_ini, y_ini, x_fin, y_fin, I1, J1, segmentos, True)
     #segmentos_G2 = DiscretizaArco(21.34, 373.15, 28.59, 380.4, 7.25, 0, segmentos, False)
     
-    print("G03",segmentos_G3)
-    #print("G02",segmentos_G2)
     
     # Crear una nueva imagen en blanco con un tamaño adecuado
     image = np.ones((800, 800, 3), dtype=np.uint8) * 255  # Agregar margen
-    image = cv2.circle(image, (int(x_ini*escala),int(y_ini+J1*escala)), 2, (0,250,0), 2) 
+    image = cv2.circle(image, (int(x_ini*escala),int(y_ini+J1*escala)), 2, (0,250,0), 2)
+    image = cv2.line(image,  (50, 50), (int(x_ini*escala),int(y_ini+J1*escala)), (0,0,250), 1)
+    x1, y1 = 50, 50
     #image = cv2.circle(image, (int((x_ini + I1)*escala),int((y_ini+J1)*escala)), int(J1*escala), (0,0,0), 2) 
-    x1, y1 = x_ini, x_fin
-    color = (0,50,0)
+    x1, y1 = x_ini, y_ini
+    color = (0,0,0)
     for segment in segmentos_G3:
-        print(segment)
         color = (color[0], color[1]+int(200/segmentos), color[2])
         image = cv2.line(image, (int(x1*escala),int(y1*escala)), (int(segment[0]*escala), int(segment[1]*escala)), (0,0,250), 1)
         x1, y1 = int(segment[0]*escala), int(segment[1]*escala)
-                
+              
     cv2.imshow("IMAGEN RESULTADO", image)
     cv2.waitKey(0)
